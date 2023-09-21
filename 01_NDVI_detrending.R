fc5197b62b2e505edc3a75c327c4ac01bc74687b
Ross Alexander <malexander@anl.gov>
2023-09-21 11:12:22 -0500

    Update 01_NDVI_detrending.R

--- 01_NDVI_detrending.R
+++ 01_NDVI_detrending.R
@@ -92,7 +92,7 @@ summary(ndvi.all2)
 
 # setting up a gamm with land cover type as a fixed effect and the satellites as a random effect
 # fitting a spline by day of year
-ndvi.gamm <- gamm(NDVI ~ type + s(doy), random=list(satellite=~1), data=ndvi.all2, na.rm=T)
+ndvi.gamm <- gamm(NDVI ~ type + s(doy, k=12), random=list(satellite=~1), data=ndvi.all2, na.rm=T)
 summary(ndvi.gamm)
 ndvi.gamm
 
