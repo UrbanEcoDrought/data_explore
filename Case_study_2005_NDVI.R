b7d29fc20fafa1bd121e74c8a0ae3ea230368507
JillianGorrell <143733950+JillianGorrell@users.noreply.github.com>
2023-12-12 13:47:04 -0500

    Update Case_study_2005_NDVI.R

--- Case_study_2005_NDVI.R
+++ Case_study_2005_NDVI.R
@@ -1,6 +1,8 @@
 # script to explore NDVI time series for the land cover classes of the Chicago region
 library(ggplot2)
 library(lubridate)
+library(mgcv)
+library(dplR)
 
 # Setting the file paths. This may be different for your computer.
 Sys.setenv(GOOGLE_DRIVE = "G:/Shared drives/Urban Ecological Drought")
@@ -24,5 +26,30 @@ unique(ndvi.sat9$year)
 #Create a subset of satellites 5 and 7
 ndvi.sats57<- subset(ndvi.all, subset = satellite %in% c("Landsat5", "Landsat7"))
 
+#Detrend data
+ndvi.sats57.gamm.step1 <- gamm(NDVI ~ type + s(doy, k=12, by=type), random=list(satellite=~1), data=ndvi.sats57, na.rm=T)
+ndvi.sats57na <- ndvi.sats57[!is.na(ndvi.sats57$NDVI),]
+ndvi.sats57na$ndvi.gam.pred.step1 <-predict(ndvi.sats57.gamm.step1)
+
+ndvi.sats57na$ndvi.gam.step1.anomaly <- ndvi.sats57na$NDVI-ndvi.sats57na$ndvi.gam.pred.step1 
+ndvi.gamm.step2 <- gam(ndvi.gam.step1.anomaly~s(doy, k=12, by=satellite), data=ndvi.sats57na, na.rm=T) 
+ndvi.sats57na$ndvi.gam.pred.step2 <- predict(ndvi.gamm.step2)
+ndvi.sats57na$ndvi.modeled <- ndvi.sats57na$ndvi.gam.pred.step1 + ndvi.sats57na$ndvi.gam.pred.step2
+
+#Create double detrend anomalies.
+ndvi.sats57na$ndvi.anomaly <- ndvi.sats57na$NDVI-ndvi.sats57na$ndvi.modeled
+head(ndvi.sats57na)
+ndvi.check.all <- merge(ndvi.sats57na, ndvi.all)
+summary(ndvi.check.all)
+
+#Parse down data frame and save.
+summary(ndvi.check.all)
+ndvi.detrend.sats57 <- ndvi.check.all[,c("year", "doy", "type", "date", "NDVI", "ndvi.modeled", "ndvi.anomaly")]
+names(ndvi.detrend.sats57) <- c("year", "doy", "type", "date", "ndvi.obs", "ndvi.modeled","ndvi.anomaly")
 
 
+#Fit gam for data
+gam.fitted.sats57 <- gam(ndvi.obs ~ ,data = ndvi.sats57, method = 'REML')
+
+ndvi.sats57na$predicted <- predict(gam.fitted.sats57)
+ndvi.sats57na$resids <- resid(gam.fitted.sats57)
