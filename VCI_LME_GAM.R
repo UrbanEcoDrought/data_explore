0f8bf50996498a970c939f7de44d791a602d9bd5
JillianGorrell <143733950+JillianGorrell@users.noreply.github.com>
2024-01-19 13:14:32 -0500

    Double Penalty
    Began using the double penalty approach to identify the best predictor variables. Subset the gam models by VCI range and land cover type.

--- VCI_LME_GAM.R
+++ VCI_LME_GAM.R
@@ -75,7 +75,7 @@ summary(ChicagolandTempSPEISPINDVIVPDNA)
 ChicagolandTempSPEISPINDVIVPDNA <- na.omit(ChicagolandTempSPEISPINDVIVPDNA)
 summary(ChicagolandTempSPEISPINDVIVPDNA)
 
-saveRDS(ChicagolandData, file.path(google.drive, "data/r_files/input_files/data_explore"))
+saveRDS(ChicagolandTempSPEISPINDVIVPDNA, file.path(google.drive, "data/data_sets/ChicagolandData"))
 
 ChicagolandTempSPEISPINDVIVPDNA$month <- lubridate::month(ChicagolandTempSPEISPINDVIVPDNA$date)
 days.use <- unique(ChicagolandTempSPEISPINDVIVPDNA$doy[ChicagolandTempSPEISPINDVIVPDNA$month >=3 & ChicagolandTempSPEISPINDVIVPDNA$month <=9])
@@ -416,3 +416,85 @@ gam.fitted.VCI.SPEI30.interactions.TMIN60d..TMAX14d.type.grow.season <- gam(VCI 
 summary(gam.fitted.VCI.SPEI30.interactions.TMIN60d.TMAX14d.type.grow.season)
 
 gam.fitted.VCI.double.penalty <- gam(VCI ~ s(year) + s(X14d.SPI) + s(X30d.SPI) + s(X60d.SPI) + s(X90d.SPI) + s(SPEI.X14d) + s(SPEI.X30d) + s(SPEI.X60d) + s(SPEI.X90d) + s(TMIN14d) + s(TMIN30d)+ s(TMIN60d)+ s(TMIN90d) + s(TMAX14d) + s(TMAX30d)+ s(TMAX60d)+ s(TMAX90d) + s(VPD) + type, data=ChicagolandGrowSeason, select=TRUE, method = 'REML')
+summary(gam.fitted.VCI.double.penalty)
+
+gam.fitted.VCI.double.penalty.wo.TMAX30 <- gam(VCI ~ s(year) + s(X14d.SPI) + s(X30d.SPI) + s(X60d.SPI) + s(X90d.SPI) + s(SPEI.X14d) + s(SPEI.X30d) + s(SPEI.X60d) + s(SPEI.X90d) + s(TMIN14d) + s(TMIN30d)+ s(TMIN60d)+ s(TMIN90d) + s(TMAX14d) + s(TMAX60d)+ s(TMAX90d) + s(VPD) + type, data=ChicagolandGrowSeason, select=TRUE, method = 'REML')
+summary(gam.fitted.VCI.double.penalty.wo.TMAX30)
+
+#running double penalty approach on GAM for data during grow season with lower VCI to perform automatic term selection
+gam.fitted.VCI.double.penalty.low.VCI <-gam(VCI ~ s(year) + s(X14d.SPI) + s(X30d.SPI) + s(X60d.SPI) + s(X90d.SPI) + s(SPEI.X14d) + s(SPEI.X30d) + s(SPEI.X60d) + s(SPEI.X90d) + s(TMIN14d) + s(TMIN30d)+ s(TMIN60d)+ s(TMIN90d) + s(TMAX14d) + s(TMAX30d) + s(TMAX60d)+ s(TMAX90d) + s(VPD) + type, data=ChicagolandGrowSeason[ChicagolandGrowSeason$VCI<=0.5,], select=TRUE, method = 'REML')
+summary(gam.fitted.VCI.double.penalty.low.VCI)
+
+gam.fitted.VCI.double.penalty.low.VCI.wo.variables <-gam(VCI ~ s(year) + s(X30d.SPI) + s(X60d.SPI) + s(SPEI.X30d) + s(SPEI.X90d) + s(TMIN14d) + s(TMIN30d)+ s(TMIN60d)+ s(TMIN90d) + s(TMAX60d)+ s(TMAX90d) + s(VPD) + type, data=ChicagolandGrowSeason[ChicagolandGrowSeason$VCI<=0.5,], select=TRUE, method = 'REML')
+summary(gam.fitted.VCI.double.penalty.low.VCI.wo.variables)
+
+gam.fitted.VCI.double.penalty.low.VCI.best.variables <-gam(VCI ~ s(year) + s(X30d.SPI) + s(TMIN30d) + s(TMIN90d) + s(TMAX60d)+ s(TMAX90d) + s(VPD) + type, data=ChicagolandGrowSeason[ChicagolandGrowSeason$VCI<=0.5,], select=TRUE, method = 'REML')
+summary(gam.fitted.VCI.double.penalty.low.VCI.best.variables)
+
+#subsetting data frame by land cover type and running GAM at different VCI ranges
+
+ChicagolandData <- readRDS(file.path(google.drive, "data/data_sets/ChicagolandData"))
+ChicagolandGrowSeason <- subset(ChicagolandData[ChicagolandData$doy>=91 & ChicagolandData$doy<=304,])
+
+gam.fitted.VCI.double.penalty.low.VCI.all.variables.urban.medium <-gam(VCI ~ s(year) + s(X14d.SPI) + s(X30d.SPI) + s(X60d.SPI) + s(X90d.SPI) + s(SPEI.X14d) + s(SPEI.X30d) + s(SPEI.X60d) + s(SPEI.X90d) + s(TMIN14d) + s(TMIN30d)+ s(TMIN60d)+ s(TMIN90d) + s(TMAX14d) + s(TMAX30d) + s(TMAX60d)+ s(TMAX90d) + s(VPD), data=ChicagolandGrowSeason[ChicagolandGrowSeason$VCI<=0.5 & ChicagolandGrowSeason$type=='urban-medium',], select=TRUE, method = 'REML')
+summary(gam.fitted.VCI.double.penalty.low.VCI.all.variables.urban.medium)
+
+gam.fitted.VCI.double.penalty.low.VCI.all.variables.urban.medium1 <-gam(VCI ~ s(year) + s(X14d.SPI) + s(X30d.SPI) + s(X60d.SPI) + s(X90d.SPI) + s(SPEI.X14d) + s(SPEI.X30d) + s(SPEI.X60d) + s(SPEI.X90d) + s(TMIN14d) + s(TMIN30d)+ s(TMIN60d)+ s(TMIN90d) + s(TMAX14d) + s(TMAX30d) + s(TMAX60d)+ s(TMAX90d) + s(VPD), data=ChicagolandGrowSeason[ChicagolandGrowSeason$VCI<=0.5 & ChicagolandGrowSeason$type=='urban-medium',], select=TRUE, method = 'REML')
+summary(gam.fitted.VCI.double.penalty.low.VCI.all.variables.urban.medium1)
+
+gam.fitted.VCI.double.penalty.low.VCI.wo.variables.urban.medium <-gam(VCI ~ s(year) + s(X30d.SPI) + s(X60d.SPI) + s(SPEI.X90d) + s(TMIN90d) + s(TMAX14d) + s(TMAX30d) + s(TMAX60d)+ s(TMAX90d) + s(VPD), data=ChicagolandGrowSeason[ChicagolandGrowSeason$VCI<=0.5 & ChicagolandGrowSeason$type=='urban-medium',], select=TRUE, method = 'REML')
+summary(gam.fitted.VCI.double.penalty.low.VCI.wo.variables.urban.medium)
+
+gam.fitted.VCI.double.penalty.lower.VCI.all.variables.urban.medium <-gam(VCI ~ s(year) + s(X14d.SPI) + s(X30d.SPI) + s(X60d.SPI) + s(X90d.SPI) + s(SPEI.X14d) + s(SPEI.X30d) + s(SPEI.X60d) + s(SPEI.X90d) + s(TMIN14d) + s(TMIN30d)+ s(TMIN60d)+ s(TMIN90d) + s(TMAX14d) + s(TMAX30d) + s(TMAX60d)+ s(TMAX90d) + s(VPD), data=ChicagolandGrowSeason[ChicagolandGrowSeason$VCI<=0.35 & ChicagolandGrowSeason$type=='urban-medium',], select=TRUE, method = 'REML')
+summary(gam.fitted.VCI.double.penalty.lower.VCI.all.variables.urban.medium)
+
+gam.fitted.VCI.double.penalty.lower.VCI.wo.variables.urban.medium <-gam(VCI ~ s(year) + s(X30d.SPI) + s(X60d.SPI) + s(X90d.SPI) + s(SPEI.X90d) + s(TMIN14d) + s(TMIN30d)+ s(TMIN60d)+ s(TMIN90d) + s(TMAX14d) + s(TMAX60d)+ s(TMAX90d) + s(VPD), data=ChicagolandGrowSeason[ChicagolandGrowSeason$VCI<=0.35 & ChicagolandGrowSeason$type=='urban-medium',], select=TRUE, method = 'REML')
+summary(gam.fitted.VCI.double.penalty.lower.VCI.wo.variables.urban.medium)
+
+gam.fitted.VCI.double.penalty.lowest.VCI.all.variables.urban.medium <-gam(VCI ~ s(year) + s(X14d.SPI) + s(X30d.SPI) + s(X60d.SPI) + s(X90d.SPI) + s(SPEI.X14d) + s(SPEI.X30d) + s(SPEI.X60d) + s(SPEI.X90d) + s(TMIN14d) + s(TMIN30d)+ s(TMIN60d)+ s(TMIN90d) + s(TMAX14d) + s(TMAX30d) + s(TMAX60d)+ s(TMAX90d) + s(VPD), data=ChicagolandGrowSeason[ChicagolandGrowSeason$VCI<=0.1 & ChicagolandGrowSeason$type=='urban-medium',], select=TRUE, method = 'REML')
+summary(gam.fitted.VCI.double.penalty.lowest.VCI.all.variables.urban.medium)
+
+gam.fitted.VCI.double.penalty.lowest.VCI.wo.variables.urban.medium <-gam(VCI ~ s(X30d.SPI) + s(X90d.SPI) + s(SPEI.X90d) + s(TMIN30d), data=ChicagolandGrowSeason[ChicagolandGrowSeason$VCI<=0.1 & ChicagolandGrowSeason$type=='urban-medium',], select=TRUE, method = 'REML')
+summary(gam.fitted.VCI.double.penalty.lowest.VCI.wo.variables.urban.medium)
+
+gam.fitted.VCI.double.penalty.low.VCI.urban.medium.interact <-gam(VCI ~ s(year) + s(X30d.SPI, TMAX30d) + s(X60d.SPI) + s(SPEI.X90d) + s(TMIN90d) + s(TMAX14d) + s(TMAX30d) + s(TMAX60d)+ s(TMAX90d) + s(VPD), data=ChicagolandGrowSeason[ChicagolandGrowSeason$VCI<=0.5 & ChicagolandGrowSeason$type=='urban-medium',], select=TRUE, method = 'REML')
+summary(gam.fitted.VCI.double.penalty.low.VCI.urban.medium.interact)
+
+gam.fitted.VCI.double.penalty.low.VCI.urban.medium.interact.doy <-gam(VCI ~ s(year) + s(X30d.SPI, TMAX30d, doy) + s(X60d.SPI) + s(SPEI.X90d) + s(TMIN90d) + s(TMAX14d) + s(TMAX30d) + s(TMAX90d) + s(VPD), data=ChicagolandGrowSeason[ChicagolandGrowSeason$VCI<=0.5 & ChicagolandGrowSeason$type=='urban-medium',], select=TRUE, method = 'REML')
+summary(gam.fitted.VCI.double.penalty.low.VCI.urban.medium.interact.doy)
+
+gam.fitted.VCI.double.penalty.low.VCI.all.variables.urban.low <-gam(VCI ~ s(year) + s(X14d.SPI) + s(X30d.SPI) + s(X60d.SPI) + s(X90d.SPI) + s(SPEI.X14d) + s(SPEI.X30d) + s(SPEI.X60d) + s(SPEI.X90d) + s(TMIN14d) + s(TMIN30d)+ s(TMIN60d)+ s(TMIN90d) + s(TMAX14d) + s(TMAX30d) + s(TMAX60d)+ s(TMAX90d) + s(VPD), data=ChicagolandGrowSeason[ChicagolandGrowSeason$VCI<=0.5 & ChicagolandGrowSeason$type=='urban-low',], select=TRUE, method = 'REML')
+summary(gam.fitted.VCI.double.penalty.low.VCI.all.variables.urban.low)
+
+gam.fitted.VCI.double.penalty.low.VCI.wo.variables.urban.low <-gam(VCI ~ s(year) + s(X30d.SPI) + s(X90d.SPI) + s(TMIN30d) + s(TMIN90d) + s(TMAX30d) + s(VPD), data=ChicagolandGrowSeason[ChicagolandGrowSeason$VCI<=0.5 & ChicagolandGrowSeason$type=='urban-low',], select=TRUE, method = 'REML')
+summary(gam.fitted.VCI.double.penalty.low.VCI.wo.variables.urban.low)
+
+gam.fitted.VCI.double.penalty.low.VCI.all.variables.urban.high <-gam(VCI ~ s(year) + s(X14d.SPI) + s(X30d.SPI) + s(X60d.SPI) + s(X90d.SPI) + s(SPEI.X14d) + s(SPEI.X30d) + s(SPEI.X60d) + s(SPEI.X90d) + s(TMIN14d) + s(TMIN30d)+ s(TMIN60d)+ s(TMIN90d) + s(TMAX14d) + s(TMAX30d) + s(TMAX60d)+ s(TMAX90d) + s(VPD), data=ChicagolandGrowSeason[ChicagolandGrowSeason$VCI<=0.5 & ChicagolandGrowSeason$type=='urban-high',], select=TRUE, method = 'REML')
+summary(gam.fitted.VCI.double.penalty.low.VCI.all.variables.urban.high)
+
+gam.fitted.VCI.double.penalty.low.VCI.wo.variables.urban.high <-gam(VCI ~ s(year) + s(X60d.SPI) + s(X90d.SPI) + s(SPEI.X60d) + s(TMIN14d) + s(TMIN30d) + s(TMAX90d) + s(VPD), data=ChicagolandGrowSeason[ChicagolandGrowSeason$VCI<=0.5 & ChicagolandGrowSeason$type=='urban-high',], select=TRUE, method = 'REML')
+summary(gam.fitted.VCI.double.penalty.low.VCI.wo.variables.urban.high)
+
+gam.fitted.VCI.double.penalty.low.VCI.all.variables.urban.open <-gam(VCI ~ s(year) + s(X14d.SPI) + s(X30d.SPI) + s(X60d.SPI) + s(X90d.SPI) + s(SPEI.X14d) + s(SPEI.X30d) + s(SPEI.X60d) + s(SPEI.X90d) + s(TMIN14d) + s(TMIN30d)+ s(TMIN60d)+ s(TMIN90d) + s(TMAX14d) + s(TMAX30d) + s(TMAX60d)+ s(TMAX90d) + s(VPD), data=ChicagolandGrowSeason[ChicagolandGrowSeason$VCI<=0.5 & ChicagolandGrowSeason$type=='urban-open',], select=TRUE, method = 'REML')
+summary(gam.fitted.VCI.double.penalty.low.VCI.all.variables.urban.open)
+
+gam.fitted.VCI.double.penalty.low.VCI.wo.variables.urban.open <-gam(VCI ~ s(X30d.SPI) + s(X60d.SPI) + s(X90d.SPI) + s(TMIN30d) + s(TMIN90d) + s(VPD), data=ChicagolandGrowSeason[ChicagolandGrowSeason$VCI<=0.5 & ChicagolandGrowSeason$type=='urban-open',], select=TRUE, method = 'REML')
+summary(gam.fitted.VCI.double.penalty.low.VCI.wo.variables.urban.open)
+
+gam.fitted.VCI.double.penalty.low.VCI.all.variables.forest <-gam(VCI ~ s(year) + s(X14d.SPI) + s(X30d.SPI) + s(X60d.SPI) + s(X90d.SPI) + s(SPEI.X14d) + s(SPEI.X30d) + s(SPEI.X60d) + s(SPEI.X90d) + s(TMIN14d) + s(TMIN30d)+ s(TMIN60d)+ s(TMIN90d) + s(TMAX14d) + s(TMAX30d) + s(TMAX60d)+ s(TMAX90d) + s(VPD), data=ChicagolandGrowSeason[ChicagolandGrowSeason$VCI<=0.5 & ChicagolandGrowSeason$type=='forest',], select=TRUE, method = 'REML')
+summary(gam.fitted.VCI.double.penalty.low.VCI.all.variables.forest)
+
+gam.fitted.VCI.double.penalty.low.VCI.wo.variables.forest <-gam(VCI ~ s(X30d.SPI) + s(SPEI.X30d) + s(TMIN30d), data=ChicagolandGrowSeason[ChicagolandGrowSeason$VCI<=0.5 & ChicagolandGrowSeason$type=='forest',], select=TRUE, method = 'REML')
+summary(gam.fitted.VCI.double.penalty.low.VCI.wo.variables.forest)
+
+gam.fitted.VCI.double.penalty.low.VCI.all.variables.crop <-gam(VCI ~ s(year) + s(X14d.SPI) + s(X30d.SPI) + s(X60d.SPI) + s(X90d.SPI) + s(SPEI.X14d) + s(SPEI.X30d) + s(SPEI.X60d) + s(SPEI.X90d) + s(TMIN14d) + s(TMIN30d)+ s(TMIN60d)+ s(TMIN90d) + s(TMAX14d) + s(TMAX30d) + s(TMAX60d)+ s(TMAX90d) + s(VPD), data=ChicagolandGrowSeason[ChicagolandGrowSeason$VCI<=0.5 & ChicagolandGrowSeason$type=='crop',], select=TRUE, method = 'REML')
+summary(gam.fitted.VCI.double.penalty.low.VCI.all.variables.crop)
+
+gam.fitted.VCI.double.penalty.low.VCI.wo.variables.crop <-gam(VCI ~ s(year) + s(X60d.SPI) + s(SPEI.X30d) + s(SPEI.X90d) + s(TMIN30d)+ s(TMIN60d) + s(TMAX60d)+ s(TMAX90d) + s(VPD), data=ChicagolandGrowSeason[ChicagolandGrowSeason$VCI<=0.5 & ChicagolandGrowSeason$type=='crop',], select=TRUE, method = 'REML')
+summary(gam.fitted.VCI.double.penalty.low.VCI.wo.variables.crop)
+
+gam.fitted.VCI.double.penalty.low.VCI.all.variables.grassland <-gam(VCI ~ s(year) + s(X14d.SPI) + s(X30d.SPI) + s(X60d.SPI) + s(X90d.SPI) + s(SPEI.X14d) + s(SPEI.X30d) + s(SPEI.X60d) + s(SPEI.X90d) + s(TMIN14d) + s(TMIN30d)+ s(TMIN60d)+ s(TMIN90d) + s(TMAX14d) + s(TMAX30d) + s(TMAX60d)+ s(TMAX90d) + s(VPD), data=ChicagolandGrowSeason[ChicagolandGrowSeason$VCI<=0.5 & ChicagolandGrowSeason$type=='grassland',], select=TRUE, method = 'REML')
+summary(gam.fitted.VCI.double.penalty.low.VCI.all.variables.grassland)
+
+gam.fitted.VCI.double.penalty.low.VCI.wo.variables.grassland <-gam(VCI ~ s(year) + s(X14d.SPI) + s(X30d.SPI) + s(X60d.SPI) + s(SPEI.X60d) + s(SPEI.X90d) + s(TMIN30d)+ s(TMIN60d) + s(TMAX60d) + s(VPD), data=ChicagolandGrowSeason[ChicagolandGrowSeason$VCI<=0.5 & ChicagolandGrowSeason$type=='grassland',], select=TRUE, method = 'REML')
+summary(gam.fitted.VCI.double.penalty.low.VCI.wo.variables.grassland)
